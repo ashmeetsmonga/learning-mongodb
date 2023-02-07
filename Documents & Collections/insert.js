6b22ab9d7288506e83b740551b4097bb8b3c5faf
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-07 23:34:07 +0530

    mongodb practise

--- Documents & Collections/insert.js
+++ Documents & Collections/insert.js
@@ -4,3 +4,21 @@ db.test.insertOne({ title: "Test Doc" }); //Inserts a single doc. If the collect
 //     acknowledged: true,
 //     insertedId: ObjectId("63e28e9513752eb40bbccc02")
 //   }
+
+doc = {
+	title: "Warm Water",
+	desc: "A bowl of warm water",
+	rating: "5",
+	type: "drink",
+	comments: [
+		{
+			name: "Ash",
+			desc: "Very warm",
+		},
+		{
+			name: "Sim",
+			desc: "Too good",
+		},
+	],
+};
+db.recipes.insertOne(doc); //Would insert the above doc in recipes collection
