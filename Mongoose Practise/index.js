f3fc57ddd7540254e87dc5d87ef780cf34194909
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-09 22:26:18 +0530

    mongoose practise setup

--- Mongoose Practise/index.js
+++ Mongoose Practise/index.js
@@ -13,4 +13,34 @@ const createDoc = async (name, completed) => {
 	}
 };
 
-createDoc("Task 1", false);
+const findDoc = async (findObject) => {
+	try {
+		const doc = await Task.find(findObject);
+		console.log(doc);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const findOneDoc = async (findObject) => {
+	try {
+		const doc = await Task.findOne(findObject);
+		console.log(doc);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const findOneAndDeleteDoc = async (findObject) => {
+	try {
+		const doc = await Task.findOneAndDelete(findObject);
+		console.log(doc);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// createDoc("Task 1", false);
+// findDoc({ completed: false });
+// findOneDoc({ completed: false });
+findOneAndDeleteDoc({ completed: false });
